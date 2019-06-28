ec7aa30fc6bbeb51672be9caa69de55b4e61607c
dglt1 <47616132+dglt1@users.noreply.github.com>
2019-06-28 13:17:57 -0400

    Update no-optimus.sh

--- switch/intel/no-optimus.sh
+++ switch/intel/no-optimus.sh
@@ -16,12 +16,12 @@ echo 'auto' > '/sys/bus/pci/devices/0000:01:00.0/power/control'  #adjust busid i
 #to find out what call disables your nvidia gpu,
 #run this (not while using nvidia gpu, this is why i left it commented out.
 #
-#` sudo /usr/share/acpi_call/examples/turn_off_gpu.sh ` 
+#` sudo /etc/switch/gpu_switch_check.sh ` 
 #
 #and see which acpi_call is returned as "works!" and then uncomment and edit this file to match.
 #same goes for the the BusID of nvidia card, 
 #default is set to 0000:01:00.0 (syntax is important) 
-#if your nvidia gpu has a 1:0:1 busID, just uncomment the line, no change needed.
+#if your nvidia gpu has a 1:00:0 busID, just uncomment the line, no change needed.
 
 #echo '\_SB.PCI0.PEG0.PEGP._OFF' > /proc/acpi/call 
 #echo -n 1 > '/sys/bus/pci/devices/0000:01:00.0/remove'
