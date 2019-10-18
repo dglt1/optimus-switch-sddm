6331ec5f43b3ba5948607880ca71ab32b7210c42
dglt1 <47616132+dglt1@users.noreply.github.com>
2019-10-18 12:11:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,6 +40,9 @@ rm -rf /etc/X11/mhwd.d/nvidia.conf.nvidia-xconfig-original
 echo 'Removing gpu configurations from /etc/X11/mhwd.d/  .......'
 rm -rf /etc/X11/xorg.conf.d/90-mhwd.conf
 rm -rf /etc/X11/xorg.conf.d/optimus.conf
+rm -rf /etc/X11/xorg.conf.d/20-intel.conf
+rm -rf /etc/X11/xorg.conf.d/nvidia.conf
+rm -rf /etc/X11/xorg.conf.d/xorg.conf
 echo 'Removing gpu configurations from /etc/X11/xorg.conf.d/ ........'
 rm -rf /etc/modprobe.d/mhwd-gpu.conf
 rm -rf /etc/modprobe.d/optimus.conf
